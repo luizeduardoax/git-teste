45199ae423841c5cacaa12fe1a551520e33bdab9
Eduardo Machado <luizaraujo.ax@gmail.com>
2020-05-22 11:38:15 -0300

    Projeto iniciado mostrando hello word e lendo um dado e impimindo na tela

---
+++ TesteGit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TesteGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string palavra = Console.ReadLine();
+            Console.WriteLine(palavra);
+        }
+    }
+}
